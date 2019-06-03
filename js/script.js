7fa4eb2e0c54a1c3027d47712b4d693ecca9102c
Flpichon <franckiiboss@gmail.com>
2019-06-03 17:01:14 +0200

    wip : pie chart

--- js/script.js
+++ js/script.js
@@ -5,27 +5,58 @@ $(document).ready (function(){
         });
     }, 1000);
 });
-var nbrCmd = [];
-var user = [];
+var nbr = [];
+var lib = [];
 $('.user-info').each(function(i) {
-   nbrCmd.push($(this).attr('cmd'));
-    user.push($(this).attr('user'));
+   nbr.push($(this).attr('cmd'));
+    lib.push($(this).attr('user'));
 })
 if (document.getElementById("pieChart")) {
 var ctxP = document.getElementById("pieChart").getContext('2d');
 var myPieChart = new Chart(ctxP, {
   type: 'pie',
   data: {
-    labels: user,
+    labels: lib,
     datasets: [{
-      data: nbrCmd,
-      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+      data: nbr,
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", "#4D5361", "#4D5362"],
+      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774","#616775","#616774"]
     }]
   },
   options: {
-    responsive: true
+    responsive: true,
+    title : {
+        display: true,
+        text: 'Nombre de commandes'
+    }
   }
 })
 }
+nbr = [];
+lib = [];
+$('.categorie-info').each(function(i) {
+    nbr.push($(this).attr('prd'));
+    lib.push($(this).attr('cat'));
+ })
+if (document.getElementById("pieChartCat")) {
+    var ctxP = document.getElementById("pieChartCat").getContext('2d');
+    var myPieChart = new Chart(ctxP, {
+      type: 'pie',
+      data: {
+        labels: lib,
+        datasets: [{
+          data: nbr,
+          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", "#4D5361", "#4D5362"],
+          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774","#616775","#616774"]
+        }]
+      },
+      options: {
+        responsive: true,
+        title : {
+            display: true,
+            text: 'Nombre de produits'
+        }
+      }
+    })
+ }
 
